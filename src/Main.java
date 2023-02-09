c2f2894927e7c0e55cdc14b420342b3996ba1ebe
orozovgr <orozovgr@mail.uc.edu>
2023-02-09 14:13:33 -0500

    Completed Lab_04_02_YearlyMaintenanceCost

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        double springCost = 22.0;
+        double summerCost = 25.0;
+        double fallCost = 30.0;
+        double winterCost = 40.0;
+        double totalCost = springCost + summerCost + fallCost + winterCost;
+        System.out.println("Spring costs is: " + springCost);
+        System.out.println("Summer cost is: " + summerCost);
+        System.out.println(" Fall cost is: " + fallCost);
+        System.out.println("Winter cost is: " + winterCost);
+        System.out.println("Total cost is: " + totalCost);
+    }
+}
